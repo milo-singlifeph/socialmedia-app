8ea09218545b370557e70b952fb736ce3977a78f
ganha <milo.somera@gmail.com>
2020-08-03 22:57:02 +0800

    Updates

---
+++ precache-manifest.d3835a2dd6d990e2bd2863b1bf97740f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b3507cf0669fdec1bdbfee63998be2",
+    "url": "/emaieloh/socialmedia-app.git/index.html"
+  },
+  {
+    "revision": "673878ab7f856d8cade3",
+    "url": "/emaieloh/socialmedia-app.git/static/css/main.0aef981a.chunk.css"
+  },
+  {
+    "revision": "5422f0133a5772a843c1",
+    "url": "/emaieloh/socialmedia-app.git/static/js/2.50f38d05.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/emaieloh/socialmedia-app.git/static/js/2.50f38d05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "673878ab7f856d8cade3",
+    "url": "/emaieloh/socialmedia-app.git/static/js/main.43959472.chunk.js"
+  },
+  {
+    "revision": "9fbcf079ca17bbe0b991",
+    "url": "/emaieloh/socialmedia-app.git/static/js/runtime-main.1f7e8e0d.js"
+  }
+]);
